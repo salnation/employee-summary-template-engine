21e3925811dac079579f31699e6e1d9456f48160
AC <adrianacovic@AC.local>
2020-08-03 19:39:00 -0400

    third commit app.js has been updated - function for employee questions have been added.

--- app.js
+++ app.js
@@ -48,6 +48,73 @@ const managerQuestions = [
     }
 ]
 
+const employeeQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter employee name:",
+        validate: async (input) => {
+            if (input == "") {
+                return "Please enter a name.";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter their email:",
+        validate: async (input) => {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                return true;
+            }
+            return "Please enter a valid email address.";
+        }
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is their role?",
+        choices: ["engineer", "intern"]
+    },
+    {
+        when: input => {
+            return input.role == "engineer"
+        },
+        type: "input",
+        name: "github",
+        message: "Engineer, enter your github username:",
+        validate: async (input) => {
+            if (input == "" || /\s/.test(input)) {
+                return "Please enter a valid GitHub username";
+            }
+            return true;
+        }
+    },
+    {
+        when: input => {
+            return input.role == "intern"
+        },
+        type: "input",
+        name: "school",
+        message: "Intern, enter your school name:",
+        validate: async (input) => {
+            if (input == "") {
+                return "Please enter a name.";
+            }
+            return true;
+        }
+    },
+    {
+        type: "list",
+        name: "addAnother",
+        message: "Add another team member?",
+        choices: ["Yes", "No"]
+    }
+]
+
+
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
